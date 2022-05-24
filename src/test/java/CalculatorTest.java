485aedfdd0be92880acf2e6656ec88b2f89debf5
bybit0408 <bybit0408@gmail.com>
2022-05-24 23:10:13 +0900

    feat : 테스트코드의 Calculator 객체 중복 선언 제거

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,21 @@
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
     @DisplayName("2와 4를 더한 값을 반환한다.")
     void add_2_4(){
-        Calculator calculator = new Calculator();
         int result = calculator.add(2, 4);
         assertThat(result).isEqualTo(6);
     }
@@ -15,7 +23,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("4와 8을 더한 값을 반환한다.")
     void add_4_8(){
-        Calculator calculator = new Calculator();
         int result = calculator.add(4, 8);
         assertThat(result).isEqualTo(12);
     }
@@ -23,7 +30,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("4에서 2를 뺀 값을 반환한다")
     void subtract_4_2(){
-        Calculator calculator = new Calculator();
         int result = calculator.subtract(4, 2);
         assertThat(result).isEqualTo(2);
     }
@@ -31,7 +37,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("25에서 5를 뺀 값을 반환한다")
     void subtract_25_5(){
-        Calculator calculator = new Calculator();
         int result = calculator.subtract(25, 5);
         assertThat(result).isEqualTo(20);
     }
@@ -39,7 +44,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("3와 4를 곱한 값을 반환한다")
     void multiply_3_4(){
-        Calculator calculator = new Calculator();
         int result = calculator.multiply(3, 4);
         assertThat(result).isEqualTo(12);
     }
@@ -47,7 +51,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("5와 2를 곱한 값을 반환한다")
     void multiply_5_2(){
-        Calculator calculator = new Calculator();
         int result = calculator.multiply(5, 2);
         assertThat(result).isEqualTo(10);
     }
@@ -55,7 +58,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("두 수를 나눈 값을 반환한다.")
     void divide_4_2(){
-        Calculator calculator = new Calculator();
         double result = calculator.divide(4, 2);
         assertThat(result).isEqualTo(2);
     }
@@ -63,7 +65,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("두 수를 나눈 값을 반환한다.")
     void divide_5_2(){
-        Calculator calculator = new Calculator();
         double result = calculator.divide(5, 2);
         assertThat(result).isEqualTo(2.5);
     }
