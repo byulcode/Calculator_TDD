e603ab86dda9d52181daaf7d3cadf58c77c1b352
bybit0408 <bybit0408@gmail.com>
2022-05-24 23:08:03 +0900

    feat : 메소드 및 테스트 코드 추가

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,71 @@
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("2와 4를 더한 값을 반환한다.")
+    void add_2_4(){
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 4);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("4와 8을 더한 값을 반환한다.")
+    void add_4_8(){
+        Calculator calculator = new Calculator();
+        int result = calculator.add(4, 8);
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("4에서 2를 뺀 값을 반환한다")
+    void subtract_4_2(){
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(4, 2);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("25에서 5를 뺀 값을 반환한다")
+    void subtract_25_5(){
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(25, 5);
+        assertThat(result).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("3와 4를 곱한 값을 반환한다")
+    void multiply_3_4(){
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(3, 4);
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("5와 2를 곱한 값을 반환한다")
+    void multiply_5_2(){
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(5, 2);
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("두 수를 나눈 값을 반환한다.")
+    void divide_4_2(){
+        Calculator calculator = new Calculator();
+        double result = calculator.divide(4, 2);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("두 수를 나눈 값을 반환한다.")
+    void divide_5_2(){
+        Calculator calculator = new Calculator();
+        double result = calculator.divide(5, 2);
+        assertThat(result).isEqualTo(2.5);
+    }
+
+}
