bef62585c7f989f139c6f0a40ed0b798e3d39c2b
byulcode <bybit0408@gmail.com>
2022-08-26 16:47:07 +0900

    feat : calculate 추상메소드 추가. 각 상수에 함수 추가

--- src/main/java/enumcalculator/EnumCalculator.java
+++ src/main/java/enumcalculator/EnumCalculator.java
@@ -1,16 +1,42 @@
 package enumcalculator;
 
-enum Operation{
-    ADD('+'), MINUS('-'), MULTIPLY('*'), DIVIDE('/');
-    private char operator;
-    Operation(char operator){
-        this.operator = operator;
+import java.util.function.BiFunction;
+
+enum OperatorType {
+    PLUS('+') {
+        public double calculate(double num1, double num2) {
+            return num1 + num2;
+        }
+    },
+    MINUS('-') {
+        public double calculate(double num1, double num2) {
+            return num1 - num2;
+        }
+    },
+    MULTIPLY('*') {
+        public double calculate(double num1, double num2) {
+            return num1 * num2;
+        }
+    },
+    DIVIDE('/') {
+        public double calculate(double num1, double num2) {
+            if (num2 == 0) {
+                throw new IllegalArgumentException("나누는 값이 0이 될 수 없습니다.");
+            }
+            return num1 / num2;
+        }
+    };
+    private char enumOperator;
+
+    OperatorType(char enumOperator) {
+        this.enumOperator = enumOperator;
     }
 
-    public char getOperator(){
-        return this.operator;
-    }
+    public abstract double calculate(double num1, double num2);
+
+
 }
 
 public class EnumCalculator {
+
 }
