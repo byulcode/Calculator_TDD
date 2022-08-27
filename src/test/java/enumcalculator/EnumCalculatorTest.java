7409ccf8b9b70aaf18dfb1a8e711cea01bad4ee6
byulcode <bybit0408@gmail.com>
2022-08-27 15:02:27 +0900

    test : calculateOperation의 2번쨰 테스트코드 추가

--- src/test/java/enumcalculator/EnumCalculatorTest.java
+++ src/test/java/enumcalculator/EnumCalculatorTest.java
@@ -24,10 +24,18 @@ class EnumCalculatorTest {
 
     @Test
     @DisplayName("7과 4를 뺀 값을 반환한다.")
-    void testCalculateOperation_7_4(){
+    void testCalculateOperation_7_4() {
         String[] sentence = {"7", "-", "4"};
         double res = enumCalculator.calculateOperation(sentence);
         assertThat(res).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("7과 4를 뺀 값을 반환한다.")
+    void testCalculateOperation_5_2() {
+        String[] sentence = {"5", "*", "2"};
+        double res = enumCalculator.calculateOperation(sentence);
+        assertThat(res).isEqualTo(10);
     }
 }
 
